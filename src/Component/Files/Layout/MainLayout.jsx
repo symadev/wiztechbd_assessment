4596a56a9a4397f022d7027254e4de1e8fd56d03
syma sultana <symasultana2002@gmail.com>
2025-12-05 16:36:21 +0600

    added some chnages

--- src/Component/Files/Layout/MainLayout.jsx
+++ src/Component/Files/Layout/MainLayout.jsx
@@ -1,17 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 
 const MainLayout = () => {
+    const location = useLocation(); 
 
-        const noHeaderFooter = location.pathname.includes('login') || 
-                       location.pathname.includes('register')
+    const noHeaderFooter = location.pathname.includes("login") || 
+                           location.pathname.includes("register");
+
     return (
         <div>
-            {noHeaderFooter || <Navbar></Navbar>}
-            <Outlet></Outlet>
-           {noHeaderFooter || <Footer></Footer>}
-            
+            {!noHeaderFooter && <Navbar />}
+            <Outlet />
+            {!noHeaderFooter && <Footer />}
         </div>
     );
 };
